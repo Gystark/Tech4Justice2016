63b71c9f2dcdad8cc60b9e7b4aaebf28ad9843bc
2019342a <theofilosalexiou@gmail.com>
2016-06-18 13:00:14 +0100

    Created initial models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,50 @@
+""" models of the project are stored here.
+All the
+"""
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Object representing a category in our database.
+    """
+
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Issue(models.Model):
+    """
+    Object representing different legal issues in our system
+    """
+
+    name = models.CharField(max_length=128, unique=True)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    """
+    Object representing the questions and answers of our system
+    """
+
+    question = models.CharField(max_length=True)
+    answer = models.TextField()
+    issue = models.ForeignKey(Issue)
+
+    def __str__(self):
+        return self.question
+
+
+class Reference(models.Model):
+    """
+    different references are stored here
+    """
+
+    reference = models.URLField()
+    question = models.ForeignKey(Question)
+
