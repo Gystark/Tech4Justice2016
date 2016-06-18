77222e8fd876825dc7a121565cc1b500a7b89549
Gystark <2209013b@student.gla.ac.uk>
2016-06-18 17:00:00 +0100

    Added search.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,9 @@ from mainapp import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.view_category, name='category'),
-    url(r'^issue/(?P<issue_name_slug>[\w\-]+)/$', views.view_issue, name='issue')
+    url(r'^issue/(?P<issue_name_slug>[\w\-]+)/$', views.view_issue, name='issue'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^dictionary/', views.dictionary, name='dictionary'),
+    url(r'^local_help/', views.local_help, name='local_help'),
+    url(r'^talk_to_someone/', views.talk_to_someone, name='talk_to_someone')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,12 +3,10 @@ from .models import Category, Reference, Issue, Question
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
 
-# Create your views here.
+
 def index(request):
-    context = {}
     categories = Category.objects.all()
-    context['categories'] = categories
-    return render(request, 'mainapp/index.html', context)
+    return render(request, 'mainapp/index.html', {'categories': categories})
 
 
 def view_category(request, category_name_slug):
@@ -17,7 +15,10 @@ def view_category(request, category_name_slug):
         category_searched = Category.objects.get(slug=category_name_slug)
         category_issues = Issue.objects.filter(category=category_searched)
         context['issues'] = category_issues
+    categories = Category.objects.all()
+    context['categories'] = categories
     return render(request, 'mainapp/issues.html', context)
+
 
 def view_issue(request, issue_name_slug):
     context = {}
@@ -25,4 +26,34 @@ def view_issue(request, issue_name_slug):
         issue_searched = Issue.objects.get(slug = issue_name_slug)
         issue_questions = Question.objects.filter(issue = issue_searched)
         context['questions'] = issue_questions
+    categories = Category.objects.all()
+    context['categories'] = categories
     return render(request, 'mainapp/questions.html', context)
+
+
+def search(request):
+    if request.GET:
+        search_term = request.GET['srch-term']
+    else:
+        search_term = ''
+    categories = Category.objects.all()
+    results = Question.objects.filter(question__contains=search_term) or \
+              Question.objects.filter(answer__contains=search_term) or \
+              Issue.objects.filter(name__contains=search_term) or \
+              Category.objects.filter(name__contains=search_term)
+    return render(request, 'mainapp/search.html', {'categories': categories, 'results': results})
+
+
+def dictionary(request):
+    categories = Category.objects.all()
+    return render(request, 'mainapp/dictionary.html', {'categories': categories})
+
+
+def local_help(request):
+    categories = Category.objects.all()
+    return render(request, 'mainapp/local_help.html', {'categories': categories})
+
+
+def talk_to_someone(request):
+    categories = Category.objects.all()
+    return render(request, 'mainapp/talk_to_someone.html', {'categories': categories})
