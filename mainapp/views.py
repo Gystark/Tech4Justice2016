6ba07ac3ca79762d4894ae04451f33484b8d3a91
Gystark <2209013b@student.gla.ac.uk>
2016-06-18 18:41:31 +0100

    Merge.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from .models import Category, Reference, Issue, Question
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import JsonResponse
+from .models import Category, Reference, Issue, Question, Dictionary
 
-# Create your views here.
+
 def index(request):
-    context = {}
     categories = Category.objects.all()
-    context['categories'] = categories
-    return render(request, 'mainapp/index.html', context)
+    return render(request, 'mainapp/index.html', {'categories': categories})
 
 
 def view_category(request, category_name_slug):
@@ -18,6 +14,7 @@ def view_category(request, category_name_slug):
         category_issues = Issue.objects.filter(category=category_searched)
         context['issues'] = category_issues
     return render(request, 'mainapp/issues.html', context)
+
 
 def view_issue(request, issue_name_slug):
     context = []
@@ -31,4 +28,41 @@ def view_issue(request, issue_name_slug):
             references = Reference.objects.filter(question=question)
             context.append({'question': question, 'references': references})
 
-    return render(request, 'mainapp/questions.html', {'context': context})
+
+def search(request):
+    if request.GET:
+        search_term = request.GET['search-term']
+    else:
+        search_term = ''
+    questions = Question.objects.filter(question__contains=search_term) or\
+                Question.objects.filter(answer__contains=search_term)
+    issues = Issue.objects.filter(name__contains=search_term)
+    categories = Category.objects.filter(name__contains=search_term)
+    return render(request, 'mainapp/search.html', {'issues': issues,'categories': categories, 'questions': questions})
+
+
+def search_dictionary(request):
+    if request.GET:
+        search_term = request.GET['search-term']
+    else:
+        search_term = ''
+    results = Dictionary.objects.filter(expression__contains=search_term) or\
+              Dictionary.objects.filter(definition__contains=search_term)
+    items = Dictionary.objects.all()
+    return render(request, 'mainapp/dictionary.html', {'results': results, 'items': items})
+
+
+def dictionary(request):
+    items = Dictionary.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'mainapp/dictionary.html', {'categories': categories, 'items': items})
+
+
+def local_help(request):
+    categories = Category.objects.all()
+    return render(request, 'mainapp/local_help.html', {'categories': categories})
+
+
+def talk_to_someone(request):
+    categories = Category.objects.all()
+    return render(request, 'mainapp/talk_to_someone.html', {'categories': categories})
