7d04a2d2b94ed56367f0339463d28e1e412d45b9
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-06-18 13:00:53 +0100

    [backend] js added

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {'text': 'Test text.'}
+    context = {'categories': ['family', 'rape', 'child abuse', 'drugs']}
     return render(request, 'mainapp/index.html', context)
