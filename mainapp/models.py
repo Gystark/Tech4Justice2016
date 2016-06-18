671f0d7eb0b515371dd3e64f805a86468da289fc
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-06-18 15:52:58 +0100

    default deleted for category slug field

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     """
 
     name = models.CharField(max_length=128, unique=True)
-    slug = models.SlugField(default='')
+    slug = models.SlugField()
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
