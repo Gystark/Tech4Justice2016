e179a6364885a703d269a0cf7370a150ec6ac174
Gystark <2209013b@student.gla.ac.uk>
2016-06-18 14:46:14 +0100

    Fixed sidebar.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,7 @@ from mainapp import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.view_category, name='category'),
+    url(r'^$', views.index, name='index'),
     url(r'^get_issues/', views.get_issues, name='get_issues')
 ]
