febf71fa12f17aa61e2c1b955e6a755a5eee379c
Gystark <2209013b@student.gla.ac.uk>
2016-06-19 11:08:47 +0100

    Categories now lead to questions.

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,24 +12,9 @@ def view_category(request, category_name_slug):
     context = {}
     if request.method == "GET":
         category_searched = Category.objects.get(slug=category_name_slug)
-        category_issues = Issue.objects.filter(category=category_searched)
-        context['issues'] = category_issues
-    return render(request, 'mainapp/issues.html', context)
-
-
-def view_issue(request, issue_name_slug):
-    context = []
-    if request.method == "GET":
-        issue_searched = Issue.objects.get(slug = issue_name_slug)
-        # get the questions related to the issue
-        issue_questions = Question.objects.filter(issue = issue_searched)
-
-        for question in issue_questions:
-            # get all the references related to this question
-            references = Reference.objects.filter(question=question)
-            context.append({'question': question, 'references': references})
-
-    return render(request, 'mainapp/questions.html', {'context': context})
+        category_questions = Question.objects.filter(issue__category=category_searched)
+        context['questions'] = category_questions
+    return render(request, 'mainapp/questions.html', context)
 
 
 def search(request):
